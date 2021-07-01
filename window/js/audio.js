c8f26d9ad64d81fd61f75e040234bdd1b1cd8c12
Hydrothermal <hydrothermal@tuta.io>
2021-07-01 02:14:17 -0400

    Add bandpass filter to audio input

--- window/js/audio.js
+++ window/js/audio.js
@@ -1,4 +1,6 @@
 const TALKING_THRESHOLD = 20;
+const FILTER_FREQUENCY = 750;
+const FILTER_STRENGTH = 0.3;
 
 let getIsTalking;
 
@@ -7,6 +9,11 @@ let getIsTalking;
     const analyser = audioContext.createAnalyser();
     analyser.smoothingTimeConstant = 0.3;
     analyser.fftSize = 1024;
+
+    const filter = audioContext.createBiquadFilter();
+    filter.Q.value = FILTER_STRENGTH;
+    filter.frequency.value = FILTER_FREQUENCY;
+    filter.type = "bandpass";
 
     const array = new Uint8Array(analyser.frequencyBinCount);
     getIsTalking = function getIsTalking() {
@@ -26,9 +33,10 @@ let getIsTalking;
         video: false,
     }).then(stream => {
         const microphone = audioContext.createMediaStreamSource(stream);
-        microphone.connect(analyser);
+        microphone.connect(filter);
+        filter.connect(analyser);
     }, (error) => {
-        console.log('Error getting audio stream');
+        console.log("Error getting audio stream");
         console.error(error);
     });
 })();
