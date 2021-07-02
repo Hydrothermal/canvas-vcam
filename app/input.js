89a5dfec384d1e4741d0dce1e8606b605e7ea2d0
David Stensland <me@terite.com>
2021-07-01 21:40:23 -0700

    Ignore key repeat

--- app/input.js
+++ app/input.js
@@ -2,6 +2,7 @@ const { EventEmitter } = require("events");
 const { app } = require("electron");
 const iohook = require("iohook");
 
+const downkeys = new Set();
 const emitter = module.exports = new EventEmitter();
 
 iohook.on("mousemove", event => {
@@ -10,6 +11,17 @@ iohook.on("mousemove", event => {
 
 for (const eventName of ["mousedown", "mouseup", "keydown", "keyup"]) {
     iohook.on(eventName, event => {
+        if(eventName == "keydown") {
+            if (downkeys.has(event.keycode)) {
+                // Ignore key repeat
+                return;
+            }
+            downkeys.add(event.keycode);
+        }
+        if(eventName == "keyup") {
+            downkeys.delete(event.keycode);
+        }
+
         emitter.emit(eventName);
     });
 }
